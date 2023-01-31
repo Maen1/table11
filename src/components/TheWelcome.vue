367f92cdf6d6752beab48b9277a027bdf24ba59a
adam maen <mk.maen93@gmail.com>
2023-01-31 16:27:53 +0100

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import WelcomeItem from "./WelcomeItem.vue";
+</script>
+
+<template>
+    <WelcomeItem>
+        <template #heading>Documentation</template>
+        <p class="text-3xl font-bold underline">
+             provides you with all information you need to get started.
+        </p>
+    </WelcomeItem>
+</template>
---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="item">
+        <i>
+            <slot name="icon"></slot>
+        </i>
+        <div class="text-5xl text-red-500  font-bold underline">
+            <h3>
+                <slot name="heading"></slot>
+            </h3>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
