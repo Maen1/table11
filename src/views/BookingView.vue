d3b32e2ab4b2e8e82384c9f76f858d5c6ad4feaa
adam maen <mk.maen93@gmail.com>
2023-02-08 19:41:07 +0100

    wip

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -21,13 +21,15 @@
 
         <div class=" flex flex-wrap md:flex-nowrap  justify-between ">
             <div class="w-full md:w-1/3 m-2 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    
-                 <svg  class="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400"  fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"></path>
-                </svg>
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-yellow-200">Need a help in Claim?</h5>
-                </a>
+                <div class="flex">
+
+                    <svg  class="w-10 h-10 mb-2 text-gray-500 dark:text-yellow-200"  fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"></path>
+                    </svg>
+                    <a href="#">
+                        <h5 class="m-2 text-2xl font-semibold tracking-tight text-yellow-200">Phone</h5>
+                    </a>
+                </div>
                 <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
                 <a href="#" class="inline-flex items-center text-blue-600 hover:underline">
                     See our guideline
@@ -35,16 +37,15 @@
                 </a>
             </div>
             <div class="w-full md:w-1/3 m-2  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <svg  class="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400"  fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"></path>
-                </svg>
+                <div class="flex">
 
-
-               
-                            
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-yellow-200">Need a help in Claim?</h5>
-                </a>
+                    <svg  class="w-10 h-10 mb-2 text-gray-500 dark:text-yellow-200"  fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"></path>
+                    </svg>
+                    <a href="#">
+                        <h5 class="m-2 ml-3 text-2xl font-semibold tracking-tight text-yellow-200">Email</h5>
+                    </a>
+                </div>
                 <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
                 <a href="#" class="inline-flex items-center text-blue-600 hover:underline">
                     See our guideline
@@ -52,13 +53,16 @@
                 </a>
             </div>          
             <div class="w-full md:w-1/3  p-6 m-2  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <svg class="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <div class="flex">
+                 <svg class="w-10 h-10 mb-2 text-gray-500 dark:text-yellow-200" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"></path>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"></path>
                 </svg>
                 <a href="#">
-                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-yellow-200">Need a help in Claim?</h5>
+                    <h5 class="m-2 text-2xl font-semibold tracking-tight text-yellow-200">Address</h5>
                 </a>
+            </div>
+               
                 <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
                 <a href="#" class="inline-flex items-center text-blue-600 hover:underline">
                     See our guideline
