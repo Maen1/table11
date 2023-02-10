912a183ab5cc46d0aba11852d89beed3f50ffaba
adam maen <mk.maen93@gmail.com>
2023-02-10 20:07:50 +0100

    change the delay time

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 AOS.init({
 	offset: 100, // offset (in px) from the original trigger point
-  	delay: 500, // values from 0 to 3000, with step 50ms
+  	delay: 200, // values from 0 to 3000, with step 50ms
   duration: 2000, 
 
 });
