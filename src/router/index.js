1e88e90519a4877f89b42c039ca21f48e5ce2592
adam maen <mk.maen93@gmail.com>
2023-02-13 20:23:08 +0100

    add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,15 @@ const router = createRouter({
             // which is lazy-loaded when the route is visited.
             component: () => import("../views/BookingView.vue"),
         },
+
+               {
+            path: "/:pathMatch(.*)*",
+            name: "404",
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import("../views/404View.vue"),
+        },
     ],
 });
 
