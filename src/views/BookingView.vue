d1cc5558bcaa98bbcf42ed8c9c0269acb6c30e4e
adam maen <mk.maen93@gmail.com>
2023-02-15 17:16:19 +0100

    update contact details css

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -31,40 +31,41 @@
 
         <div class=" flex flex-wrap md:flex-nowrap  justify-between  mt-8">
             <div data-aos="fade-down" class="w-full md:w-1/3 m-2 p-6 bg-white  rounded-lg md:rounded-xl shadow dark:bg-gray-900 dark:border-gray-700">
-                <div class="flex justify-center">
+                <div class="flex justify-left">
                     <svg  class="w-10 h-10 mb-2 text-gray-500  dark:text-yellow-200"  fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"></path>
                     </svg>
                     <div>
-                        <h5 class="m-2 text-xl font-semibold text-yellow-200">Phone:  </h5>
-                         <a href="tel:02085700164" class="text-gray-200">02085700164</a>
-                         <a href="tel:07771819555" class="text-gray-200">07771819555</a>
+                        <h5 class="m-2 text-xl font-semibold text-yellow-200">Phone  </h5>
+                         <a  href="tel:02085700164" class="text-gray-200 ml-8">02085700164</a> 
+                            <br/>
+                         <a  href="tel:07771819555" class="text-gray-200 ml-8 mt-4">07771819555</a>
                     </div>
                 </div>
             </div>
 
-            <div data-aos="fade-down" class="w-full md:w-1/3 m-2  p-6 bg-white  rounded-lg md:rounded-xl shadow dark:bg-gray-900 dark:border-gray-700">
-                <div class="flex justify-center">
+            <div data-aos="fade-down" class="w-full md:w-1/3  p-6 m-2  bg-white  rounded-lg md:rounded-xl shadow dark:bg-gray-900 dark:border-gray-700">
+                <div class="flex justify-left">
 
                     <svg  class="w-10 h-10 mb-2 text-gray-500 dark:text-yellow-200"  fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"></path>
                     </svg>
                       <a href="mailto:shisha.table11@gmail.com">
-                        <h5 class="m-2 text-xl font-semibold text-yellow-200">Email:  </h5>
-                         <p class="text-gray-200">shisha.table11@gmail.com</p>
+                        <h5 class="m-2 text-xl font-semibold text-yellow-200">Email </h5>
+                         <p class="text-gray-200 ml-8">shisha.table11@gmail.com</p>
                     </a>
                 </div>
             </div>          
             <div data-aos="fade-down" class="w-full md:w-1/3  p-6 m-2  bg-white  rounded-lg md:rounded-xl shadow dark:bg-gray-900 dark:border-gray-700">
-            <div class="flex justify-center">
+            <div class="flex justify-left">
                  <svg class="w-10 h-10 mb-2 text-gray-500 dark:text-yellow-200" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"></path>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"></path>
                 </svg>
                   <a href="https://maps.app.goo.gl/?link=https://www.google.com/maps/place/Table%2B11,%2B142-144%2BHigh%2BSt,%2BHounslow%2BTW3%2B1LR/data%3D!4m2!3m1!1s0x48760d3ca8eed589:0x9eed07bd8fb2aecd?utm_source%3Dmstt_1%26entry%3Dgps&apn=com.google.android.apps.maps&afl=https://www.google.com/maps/place/Table%2B11,%2B142-144%2BHigh%2BSt,%2BHounslow%2BTW3%2B1LR/data%3D!4m2!3m1!1s0x48760d3ca8eed589:0x9eed07bd8fb2aecd?utm_source%3Dmstt_1%26entry%3Dgps&ibi=com.google.Maps&ius=comgooglemapsurl&isi=585027354&ifl=https://www.google.com/maps/place/Table%2B11,%2B142-144%2BHigh%2BSt,%2BHounslow%2BTW3%2B1LR/data%3D!4m2!3m1!1s0x48760d3ca8eed589:0x9eed07bd8fb2aecd?utm_source%3Dmstt_1%26entry%3Dgps">
-                        <h5 class="m-2 text-xl font-semibold text-yellow-200">Location:  </h5>
+                        <h5 class="m-2 text-xl font-semibold text-yellow-200">Location </h5>
                         
-                        <p class="text-gray-200">
+                        <p class="text-gray-200 ml-8">
                              42-144 Hounslow High Street, Hounslow, London, UK 
                                 <br>
                              TW3 1LR
