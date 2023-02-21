1c8391c27b001c41720624f49f208957ed38520b
adam maen <mk.maen93@gmail.com>
2023-02-21 12:57:34 +0100

    firx data-aos margin space

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,11 +5,11 @@
 <template>
     <div>
         <section class="md:-mt-5">
-            <div data-aos="fade-up" class="relative overflow-hidden bg-no-repeat bg-cover">
-            <img class=" h-[600px] w-full" src="../assets/img/shisha4.jpg" alt="shisha">
+            <div data-aos="fade-up" class="aos-init relative overflow-hidden bg-no-repeat bg-cover">
+                 <img class=" h-[600px] w-full" src="../assets/img/shisha4.jpg" alt="shisha">
             </div>
 
-            <div data-aos="zoom-in" class="container mx-auto px-6 md:px-12 xl:px-32">
+            <div data-aos="zoom-in" class="aos-init container mx-auto px-6 md:px-12 xl:px-32">
             <div class="text-center text-yellow-800">
                 <div class="block rounded-xl shadow-lg px-6 py-12 md:py-16 md:px-12 bg-gray-900" style="margin-top: -210px; backdrop-filter: blur(30px);">
                 <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">The Best Shisha in Town<br /><span class="text-gray-600">Welcome to Table11</span></h1>
@@ -23,7 +23,7 @@
 
         <div class="mt-10">
             <div class="relative flex justify-center flex flex-wrap mt-10  md:m-10">
-                <div data-aos="fade-right" class="max-w-lg bg-white  rounded-xl shadow dark:bg-gray-900 dark:border-gray-700 m-6 ">
+                <div data-aos="fade-right" class="aos-init max-w-sm md:max-w-lg bg-white  rounded-xl shadow dark:bg-gray-900 dark:border-gray-700 m-6 ">
                     <a href="#">
                         <img class="rounded-xl hover:-translate-y-1 hover:scale-105 duration-300" src="../assets/img/orange_juice.jpg" alt="" />
                     </a>
@@ -31,7 +31,7 @@
                         <a href="#">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Fresh Juice</h5>
                         </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest juice ever ....</p>
                         <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-xl hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Read more
                             <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -39,7 +39,7 @@
                     </div>
                 </div>
                 
-                <div data-aos="fade-left" class="max-w-lg bg-white  rounded-xl shadow dark:bg-gray-900 dark:border-gray-700 m-6">
+                <div data-aos="fade-up" class="aos-init max-w-sm md:max-w-lg bg-white  rounded-xl shadow dark:bg-gray-900 dark:border-gray-700 m-6">
                     <a href="#">
                         <img class="rounded-xl hover:-translate-y-1 hover:scale-105 duration-300" src="../assets/img/fest.jpg" alt="" />
                     </a>
@@ -47,7 +47,7 @@
                         <a href="#">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Delicious Food</h5>
                         </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the most delicious food....</p>
                         <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-xl hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Read more
                             <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -60,10 +60,10 @@
 
     
     <section class="relative bg-gray-900 mt-4">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="grid max-w-screen-xl px-4 py-8 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl dark:text-white">Best Shisha ever </h1>
-                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Some bullshit to write here ....</p>
+                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">...</p>
     
                 <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     Reserve your table 
@@ -75,15 +75,14 @@
         </div>
     </section>
 
-     <header>
-            <div
+     <div
             data-aos="zoom-in"
             id="carouselExampleCrossfade"
-            class="carousel slide carousel-fade relative my-5"
+            class="aos-init carousel slide carousel-fade relative my-5"
             data-bs-ride="carousel"
             data-carousel="dynamic"
             >
-                <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center">
+                <div class="aos-init carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center">
                     <button
                     type="button"
                     data-bs-target="#carouselExampleCrossfade"
@@ -128,26 +127,7 @@
                         />
                     </div>
                 </div>
-                <!-- <button
-                    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-                    type="button"
-                    data-bs-target="#carouselExampleCrossfade"
-                    data-bs-slide="prev"
-                    >
-                    <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button
-                class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-                type="button"
-                data-bs-target="#carouselExampleCrossfade"
-                data-bs-slide="next"
-                >
-                <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button> -->
         </div>
-        </header>
 
 
     <!-- clients -->
