36c8640be94c966dd7d6b19ae97814433455e6f6
adam maen <mk.maen93@gmail.com>
2023-02-21 18:40:57 +0100

    wip

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -60,17 +60,19 @@
 
     
     <section class="relative bg-gray-900 mt-4">
-        <div class="grid max-w-screen-xl px-4 py-8 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7">
-                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl dark:text-white">Best Shisha ever </h1>
+        <div class="md:flex flex-warp max-w-screen-xl px-4 py-8">
+            <div class="place-self-center text-center w-full md:w-1/2">
+                <h1 class="max-w-xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl dark:text-white">
+                    Best Shisha ever 
+                </h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">...</p>
     
                 <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     Reserve your table 
                 </a> 
             </div>
-            <div class="mt-5 lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="../assets/img/shisha_web.png" alt="shishas">
+            <div class="reltaive place-self-center text-center  w-full md:w-1/2">
+                <img class="float-right" src="../assets/img/shisha_web.png" alt="shishas">
             </div>                
         </div>
     </section>
