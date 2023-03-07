451f02be919e635cb09c2f9bf5721713f03e4145
adam maen <mk.maen93@gmail.com>
2023-03-07 19:33:09 +0100

    update menu

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,16 +1,13 @@
 import Footer from "./components/Footer.vue";
 
 <template>
-	<div class="relative menu">
-		<section class="overflow-hidden text-white-700 h-screen">
-			<div class="container px-5 py-2 mx-auto">
-				<div data-aos="zoom-out" class="flex flex-wrap -m-1 md:-m-2">
-					<div class="w-full 2 p-1 md:p-2">
-							<img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+	<div class="relative menu md:mb-8">
+		<section class="overflow-hidden text-white-700 h-screen grid justify-items-center">
+			<div class="container">
+				<div data-aos="zoom-out" class="grid  justify-items-center">
+							<img alt="gallery" class="rounded-lg"
 							src="../assets/img/menu.jpg">
 					</div>
-                    
-				</div>
 			</div>
 		</section>
 
