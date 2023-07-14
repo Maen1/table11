757aafbdc51109353f4b9a97ebaf9d5ec937d453
maen <mk.maen93@gmail.com>
2023-07-14 13:25:54 +0200

    add contact component

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,16 +4,16 @@ import { RouterLink, RouterView } from "vue-router";
 
 
 <template>
-<div class="sticky top-0  z-50">
-     <nav
+    <div class="sticky top-0  z-50">
+        <nav
             class="relative mb-2 mx-w-full flex flex-wrap items-center justify-between  bg-zinc-900 text-gray-200 shadow-lg navbar navbar-expand-lg navbar-light"
             >
-             
+
             <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
                 <div>
-                        <RouterLink to="/">
-                        <img class="w-8 m-0 m-2" src="../assets/img/table11_logo_p.png" alt="table11 logo">
-                        </RouterLink>
+                    <RouterLink to="/">
+                    <img class="w-8 m-0 m-2" src="../assets/img/table11_logo_p.png" alt="table11 logo">
+                    </RouterLink>
                 </div>
                 <div  class="md:hidden flex space-x-6 sm:justify-center ">
                     <a href="https://instagram.com/shisha_table11?igshid=YmMyMTA2M2Y=" class="text-primary hover:text-slate-800 dark:hover:text-amber-500">
@@ -33,53 +33,53 @@ import { RouterLink, RouterView } from "vue-router";
                     </a>
                 </div>
                 <button
-                class="navbar-toggler text-primary border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent1"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                >
-                <svg
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fas"
-                    data-icon="bars"
-                    class="w-6"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 448 512"
-                >
-                    <path
-                    fill="currentColor"
-                    d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-                    ></path>
-                </svg>
+                    class="navbar-toggler text-primary border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent1"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    >
+                    <svg
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="bars"
+                        class="w-6"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 448 512"
+                        >
+                        <path
+                            fill="currentColor"
+                            d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+                            ></path>
+                    </svg>
                 </button>
                 <div class="collapse navbar-collapse flex-grow items-center text-primary"  id="navbarSupportedContent1">
-                
-                <!-- Left links -->
-                <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto md:ml-3">
-                    <li class="nav-item p-2">
-                     <RouterLink to="/">Home</RouterLink>
-                    </li>
-                    <li class="nav-item p-2">
-                     <RouterLink to="/gallery">Gallery</RouterLink>
-                    </li>
-                    <li class="nav-item p-2">
-                     <RouterLink to="/menu">Menu</RouterLink>
-                    </li>
-                    <li class="nav-item p-2">
-                     <RouterLink to="/booking">Booking</RouterLink>
-                    </li>
-                </ul>
-                <!-- Left links -->
+
+                    <!-- Left links -->
+                    <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto md:ml-3">
+                        <li class="nav-item p-2">
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                        <li class="nav-item p-2">
+                            <RouterLink to="/gallery">Gallery</RouterLink>
+                        </li>
+                        <li class="nav-item p-2">
+                            <RouterLink to="/menu">Menu</RouterLink>
+                        </li>
+                        <li class="nav-item p-2">
+                            <RouterLink to="/booking">Booking</RouterLink>
+                        </li>
+                    </ul>
+                    <!-- Left links -->
                 </div>
                 <!-- Collapsible wrapper -->
             </div>
-            </nav>
-</div>
+        </nav>
+    </div>
 </template>
 
 <style scoped>
