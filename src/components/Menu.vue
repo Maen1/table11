32d7837516dc2546ea90f521a3e332c23a8eaa72
maen <mk.maen93@gmail.com>
2023-07-18 23:11:35 +0200

    wip

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,14 +3,12 @@
 </script>
 <template>
 	<div class="relative menu md:mb-8 px-6 md:px-8 xl:px-24">
-		<section class="overflow-hidden text-white-700 h-screen grid justify-items-center">
 			<div class="container">
 				<div data-aos="zoom-out" class="grid  justify-items-center">
 							<img alt="gallery" class="rounded-lg"
 							src="../assets/img/menu.jpg">
 					</div>
 			</div>
-		</section>
 	</div>
 
 </template>
