199b41c89298a3f9027021687b3293f7126ab89a
maen <mk.maen93@gmail.com>
2023-08-02 10:11:42 +0200

    wip

---
+++ dist/assets/MenuView-518de431.js
@@ -0,0 +1,1 @@
+import{o as e,c,d as s,M as t}from"./index-ccf243fe.js";const a={class:"container h-screen"},i={__name:"MenuView",setup(n){return(o,r)=>(e(),c("div",a,[s(t)]))}};export{i as default};
--- dist/assets/MenuView-ba556255.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c,d as s,M as t}from"./index-39a6ed9d.js";const a={class:"container h-screen"},i={__name:"MenuView",setup(n){return(o,r)=>(e(),c("div",a,[s(t)]))}};export{i as default};
