541e036f3d8ad1aa6d076799a3f65c829a705090
maen <mk.maen93@gmail.com>
2023-08-02 10:11:23 +0200

    add clients components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Gallery from "./Gallery.vue";
-
+import Clients from "./Clients.vue";
 </script>
 
 <template>
@@ -74,65 +74,11 @@ import Gallery from "./Gallery.vue";
             </div>
         </div>
 
-
-
-    
         <!-- Gallery -->
         
         <Gallery />
-
         <!-- clients -->
-
-        <div data-aos="zoom-out" class="aos-init relative grid py-3 px-6 md:px-12 xl:px-24  rounded-xl shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2">
-            <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-primary rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-zinc-900 dark:border-gray-700">
-                <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-                    <h3 class="text-lg font-semibold text-slate-800 dark:text-accent">Best Shisha Ever</h3>
-                    <p class="my-4 font-light">If you care for your shihsa, you should visit table11."</p>
-                </blockquote>
-                <figcaption class="flex items-center justify-center space-x-3">
-                    <img class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture">
-                    <div class="space-y-0.5 font-medium dark:text-accent text-left">
-                        <div>Bonnie Green</div>
-                    </div>
-                </figcaption>
-            </figure>
-            <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-primary rounded-tr-lg dark:bg-zinc-900 dark:border-gray-700">
-                <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-                    <h3 class="text-lg font-semibold text-slate-800 dark:text-accent">Amazing juice</h3>
-                    <p class="my-4 font-light">If you care for your shihsa, you should visit table11."</p>
-                </blockquote>
-                <figcaption class="flex items-center justify-center space-x-3">
-                    <img class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png" alt="profile picture">
-                    <div class="space-y-0.5 font-medium dark:text-accent text-left">
-                        <div>Roberta Casas</div>
-                    </div>
-                </figcaption>    
-            </figure>
-            <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-primary rounded-bl-lg md:border-b-0 md:border-r dark:bg-zinc-900 dark:border-gray-700">
-                <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-                    <h3 class="text-lg font-semibold text-slate-800 dark:text-accent">Mindblowing environment</h3>
-                    <p class="my-4 font-light">If you care for your shihsa, you should visit table11."</p>
-                </blockquote>
-                <figcaption class="flex items-center justify-center space-x-3">
-                    <img class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="profile picture">
-                    <div class="space-y-0.5 font-medium dark:text-accent text-left">
-                        <div>Jese Leos</div>
-                    </div>
-                </figcaption>
-            </figure>
-            <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-primary rounded-b-lg md:rounded-br-lg dark:bg-zinc-900 dark:border-gray-700">
-                <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-                    <h3 class="text-lg font-semibold text-slate-800 dark:text-accent">Efficient Staff</h3>
-                    <p class="my-4 font-light">If you care for your shihsa, you should visit table11."</p>
-                </blockquote>
-                <figcaption class="flex items-center justify-center space-x-3">
-                    <img class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="profile picture">
-                    <div class="space-y-0.5 font-medium dark:text-accent text-left">
-                        <div>Joseph McFall</div>
-                    </div>
-                </figcaption>
-            </figure>
-        </div>
+        <clients />
     </div>
 </template>
 
