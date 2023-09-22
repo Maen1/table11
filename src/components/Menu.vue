87c24c72c2d2f1d6a4d28858bea9d4da6e233a4d
maen1 <mk.maen93@gmail.com>
2023-09-22 22:09:15 +0200

    add menu pdf

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,10 +2,10 @@
 <script setup>
 </script>
 <template>
-	<section class="relative md:mb-8 px-6 md:px-44">
-			<div class="grid justify-items-center">
-						<img alt="gallery" class="rounded-lg"
-						src="../assets/img/menu.jpg">
+	<section class="relative md:mb-8 -mt-12 md:-mt-20 px-6 md:px-44">
+		<div class="grid justify-items-center">
+			<!-- <img alt="gallery" class="rounded-lg" src="../assets/img/menu.jpg"> -->
+			<iframe class="h-screen w-full" type="application/pdf"   src="src/assets/img/menu.pdf"> test</iframe>
 		</div>
 	</section>
 </template>
